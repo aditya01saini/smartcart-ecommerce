e62b19b4ea6c47277ceb92da4c0117ea8e814ab9
Dev aadi <adityasaini35947@gmail.com>
2026-02-09 12:28:50 +0530

    add controllers folder

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,8 @@
+import ErrorHandler from "../middlewares/errorMiddleware.js";
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
+import database from "../database/db.js";
+
+export const register = catchAsyncErrors(async(req, res, next) => {});
+export const login = catchAsyncErrors(async(req, res, next) => {});
+export const getUser = catchAsyncErrors(async(req, res, next) => {});
+export const logut = catchAsyncErrors(async(req, res, next) => {});
