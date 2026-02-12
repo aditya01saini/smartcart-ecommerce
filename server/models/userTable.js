61632b59041cc3811e282ae0389dc4cf4eb18e4d
Dev aadi <adityasaini35947@gmail.com>
2026-02-12 21:42:53 +0530

    update userTable model

--- server/models/userTable.js
+++ server/models/userTable.js
@@ -8,14 +8,15 @@ export async function createUserTable() {
             name VARCHAR(100) NOT NULL CHECK (char_length(name) >= 3),
             email VARCHAR(100) UNIQUE NOT NULL,
             password Text NOT NULL,
-            role VARCHAR(10) DEFAULT "User" CHECK (role IN ("User", "Admin")),
-            avtar JSONB DEFAULT NULL,
+            role VARCHAR(10) DEFAULT 'User' CHECK (role IN ('User', 'Admin')),
+            avatar JSONB DEFAULT NULL,
             reset_password_token TEXT DEFAULT NULL,
             reset_password_expire TIMESTAMP DEFAULT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
         );
             `;
+            await database.query(query);
   } catch (error) {
     console.error("Error creating user table:", error);
     process.exit(1);
