7f53cecabcb3bb2fd5601e49796836f8f45cf954
Dev aadi <adityasaini35947@gmail.com>
2026-02-12 21:43:15 +0530

    update authRouter

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { register, login,  getUser, logout } from "../controllers/authControllers.js";
+import { register, login,  getUser, logout, forgetPassword, resetPassword, updatePassword, updateProfile } from "../controllers/authControllers.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", logout);
-router.get("/me", getUser);
+router.get("/me",isAuthenticated, getUser);
+router.get("/logout",isAuthenticated, logout);
+router.post("/password/forgot", forgetPassword);
+router.put("/password/reset/:token",resetPassword)
+router.put("/password/update",isAuthenticated, updatePassword);
+router.put("/profile/update",isAuthenticated, updateProfile);
+
+
 
 export default router;
