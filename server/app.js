6481a7f211ac2994f830d6c74f5b63d772133917
Dev aadi <adityasaini35947@gmail.com>
2026-02-17 17:33:14 +0530

    update app.js file

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import authRouter from "./router/authRoutes.js";
 import productRouter from "./router/productRoutes.js";
 import adminRouter from "./router/adminRoutes.js";
-
+import database from "./database/db.js";
 
 const app = express();
 
@@ -20,6 +20,67 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
+);
+
+app.post(
+  "/api/v1/payment/webhook",
+  express.row({ type: "application/json" }),
+  async (req, res) => {
+    const sig = req.headers["stripe-signture"];
+    let event;
+    try {
+      event = Stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET,
+      );
+    } catch (error) {
+      return res.status(400).send(`webhook Error: ${error.message || error}`);
+    }
+
+    //handling the event
+
+    if (event.type === "payment_intent.succeeded") {
+      const paymentIntent_client_secret =
+        event.data.object.paymentIntent_client_secret;
+
+      //finding and update payment
+
+      try {
+        const updatePaymentStatus = "paid";
+        const paymentTableUpdateResult = await database.query(
+          `UPDATE payment SET payment_status = $1 WHERE payment_intent_id = $2 RETURNING *`,
+          [updatePaymentStatus, paymentIntent_client_secret],
+        );
+        const orderTableUpdateResult = await database.query(
+          `UPDATE orders SET paid_at = NOW() WHERE id = $1 RETURNING *`,
+          [paymentTableUpdateResult.rows[0].order_id],
+        );
+
+        //reduce stock for each product
+        const orderId = paymentTableUpdateResult.rows[0].order_id;
+
+        const { rows: orderedItems } = await database.query(
+          `SELECT product_id, quantity FROM order_item WHERE order_id  = $1`,
+          [orderId],
+        );
+
+        //for each ordered item reduce the product stock
+
+        for (const item of orderedItems) {
+          await database.query(
+            `UPDATE products SET stock = stock - $1 WHERE id = $2`,
+            [item.quantity, item.product_id],
+          );
+        }
+      } catch (error) {
+        return res
+          .status(500)
+          .send("Error updating paid_at timestemp in orders table");
+      }
+    }
+    res.status(200).send({ received: true });
+  },
 );
 
 app.use(cookieParser());
@@ -35,9 +96,7 @@ app.use(
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/product", productRouter);
-app.use("/api/v1/admin", adminRouter
-);
-
+app.use("/api/v1/admin", adminRouter);
 
 createTables();
 app.use(errorMiddleware);
